4131b429a9ec108d60c0e700dc753fb1210f3d4b
SamSathish <satishpaladi11@gmail.com>
2021-11-26 00:45:54 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,11 +8,6 @@ variable "gcp_project" {
   type        = string
   description = "GCP project name"
 }
-# GCP authentication file
-variable "gcp_auth_file" {
-  type        = string
-  description = "GCP authentication file"
-}
 variable "bucket-name" {
   type        = string
   description = "The name of the Google Storage Bucket to create"
